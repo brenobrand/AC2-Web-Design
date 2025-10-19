0ba55b0549dba648d6bc2ff741d37ee18c80b927
FelipeFVM <fevemorais@gmail.com>
2025-10-19 13:34:22 -0300

    Create calculadora.js

---
+++ js/calculadora.js
@@ -0,0 +1,13 @@
+// js/calculadora.js
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Pega os elementos do formulário e do resultado pelo ID
+    const calculatorForm = document.getElementById('calculatorForm');
+    const salarioInput = document.getElementById('salarioInput');
+    const precoInput = document.getElementById('precoInput');
+    const horasMensaisInput = document.getElementById('horasMensaisInput');
+    const resultadoCard = document.getElementById('resultadoCard');
+    const resultadoHoras = document.getElementById('resultadoHoras');
+    const resultadoDias = document.getElementById('resultadoDias');
+    const resultadoValorHora = document.getElementById('resultadoValorHora');
